02b91fb883917cbc290d50e8ac82fbfe58be5f15
Amanda Martins <amanda_nelva07@hotmail.com>
2024-05-28 21:42:39 -0300

    primeiro commit

---
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,9 @@
+public class ParametrosInvalidosException extends Exception{
+
+    public ParametrosInvalidosException(String string) {
+        //TODO Auto-generated constructor stub
+
+        System.out.println("O segundo parâmetro deve ser maior que o primeiro.");
+    }
+
+}
